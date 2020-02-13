ecf4efdb2ee8e543ca6cb69a2dddbf0e39526f8e
dalabarda <dalabarda@gmail.com>
2020-02-13 11:06:30 +0100

    6 files modified

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -6,6 +6,7 @@ import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
 export class DropdownDirective {
   @HostBinding('class.show') isOpen = false;
   @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    // if the cursor is NOT deactivated (not-allowed) AND
     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
   constructor(private elRef: ElementRef) {}
