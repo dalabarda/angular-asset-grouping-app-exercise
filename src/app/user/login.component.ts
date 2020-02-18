388d4669ac30433356d91e76edc8069db996a704
dalabarda <dalabarda@gmail.com>
2020-02-18 18:16:48 +0100

    1 file modified

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -13,11 +13,11 @@ export class LoginComponent {
 
     login(formValues: any) {
       this.authService.loginUser(formValues.userName, formValues.password)
-      this.router.navigate(['events'])
+      this.router.navigate(['assets'])
     }
 
     cancel() {
-      this.router.navigate(['events'])
+      this.router.navigate(['assets'])
   }
   
 }
