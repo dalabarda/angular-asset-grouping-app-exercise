b8f042a181c8c64318a8c9ce4814515300cfea1a
dalabarda <dalabarda@gmail.com>
2020-03-01 18:15:44 +0100

    1 file modified

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,6 @@
 // this is just to fake a server!!!!
 export class AuthService_2 {
-  loggedIn = true; //TODO it must be false
+  loggedIn = false; //TODO
 
   // just to fake it will take a couple of time to finish
   isAuthenticated() {
@@ -20,26 +20,3 @@ export class AuthService_2 {
     this.loggedIn = false;
   }
 }
-
-
-// @Injectable()  
-// export class AuthService {
-//   currentUser:IUser
-//   loginUser(userName: string, password: string) {
-//     this.currentUser = {
-//       id: 1,
-//       userName: userName,
-//       firstName: 'Joohn',
-//       lastName: 'Papa' // if there is parameter wrong in 'user.model' it will be highlighted here
-//     }
-//   }
-
-//   isAuthenticated() {
-//     return !!this.currentUser;
-//   }
-
-//   updateCurrentUser(firstName:string, lastName:string) {
-//     this.currentUser.firstName = firstName
-//     this.currentUser.lastName = lastName
-//   }
-// }
