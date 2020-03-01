4d9ef96144264be088ba2908713513337802a09f
dalabarda <dalabarda@gmail.com>
2020-03-01 18:15:12 +0100

    last commit today

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,6 @@
 // this is just to fake a server!!!!
 export class AuthService_2 {
-  loggedIn = f; //TODO
+  loggedIn = true; //TODO it must be false
 
   // just to fake it will take a couple of time to finish
   isAuthenticated() {
