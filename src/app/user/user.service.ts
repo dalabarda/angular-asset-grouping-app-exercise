026d678dfb8a4b3121d35ead85632f325bd1ed87
dalabarda <dalabarda@gmail.com>
2020-03-08 23:27:34 +0100

    removed useled code.

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,7 +1,6 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable,  } from '@angular/core';
 
 @Injectable({ providedIn: 'root'})
 export class UserService {
-  activatedEmitter = new EventEmitter<boolean>();
 
 }
