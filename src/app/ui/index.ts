5dbf14f8ff5f7921a453f66d565a925084efdd6a
dalabarda <dalabarda@gmail.com>
2020-03-11 12:49:11 +0100

    1 file modified, 1 added, 1 deleted

--- src/app/ui/index.ts
+++ src/app/ui/index.ts
@@ -2,7 +2,7 @@ export * from './asset-item.component';
 export * from './asset-lists.component';
 export * from './assetsWrapper.component';
 export * from './assets-lists-resolver.service';
-export * from './unless.directive';
+// export * from './unless.directive';
 
 
 export * from './asset-details/index';
